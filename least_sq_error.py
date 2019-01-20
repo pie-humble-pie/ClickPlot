9a56a94a755ec1d75fe1e6650836b10735912633
pie-humble-pie <arya.m0ha1@gmail.com>
2019-01-20 16:59:10 +0530

    stochastic_gd added

--- least_sq_error.py
+++ least_sq_error.py
@@ -1,20 +1,47 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def hypothesis(x,theta):
+    return theta[0] + theta[1]*x
+
+def gradient(X,Y,theta):
+
+    grad = np.zeros((2,))
+    m = X.shape[0]
+
+    for i in range(m):
+        hx = hypothesis(X[i],theta)
+        grad[0] +=  (hx-Y[i])
+        grad[1] += (hx-Y[i])*X[i]
+
+    return grad
+
+
+def gradientDescent(X,Y,learning_rate=0.01):
+
+    theta = np.array([0.0,0.0])
+
+    itr = 0
+    max_itr = 100
+
+
+    while(itr<=max_itr):
+        grad = gradient(X,Y,theta)
+        print("%d %d\n"%(grad[0],grad[1]))
+        theta[0] = theta[0] - learning_rate*grad[0]
+        theta[1] = theta[1] - learning_rate*grad[1]
+        itr += 1
+    return theta
+
 def plot_hypothesis(X, Y, N):
 
      global ax1,ax2,r,R
-     num = den = 0
-     xm = np.mean(X)
-     ym = np.mean(Y)
-
-     for i in range(N):
-         num += (X[i] - xm) * (Y[i] - ym)
-         den += (X[i] - xm) ** 2
-
-     m = num/den
-     c = ym - m*xm
-     R.append(Y[-1] - m*X[-1] - c)
+     final_theta = gradientDescent(X,Y)
+     c = final_theta[0]
+     m = final_theta[1]
+     #print("c = %d m = %d"%(c,m))
+     R = np.append(R,Y[-1] - m*X[-1] - c)
      for i in range(N):
          if r[i] != 0:
             r[i].remove()
@@ -32,21 +59,21 @@ def draw(m,c):
 
 def onclick(event):
     global X,Y,N,r
-    X.append(event.xdata)
-    Y.append(event.ydata)
-    r.append(0)
+    X = np.append(X,event.xdata)
+    Y = np.append(Y,event.ydata)
+    r = np.append(r,0)
     print("X = %d , Y = %d" %(X[-1],Y[-1]))
     ax1.scatter(X,Y,color = 'blue',label = 'Data Points')
     N += 1
     if N == 1:
-        R.append(0)
+        R = np.append(R,0)
     if N >= 2:
         plot_hypothesis(X,Y,N)
 
-X = []
-Y = []
-r = []
-R = []
+X = np.array([])
+Y = np.array([])
+r = np.array([])
+R = np.array([])
 N = 0
 fig = plt.figure()
 ax1 = fig.add_subplot(1,2,1)
